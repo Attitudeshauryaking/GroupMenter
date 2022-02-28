d969318dc71544869c3047ed6b236e604a10fa0e
Attitude Shaurya king <86145370+DevilXD1@users.noreply.github.com>
2022-02-28 12:03:18 +0530

    Update __musicals.py

--- GroupMenter/modules/__musicals.py
+++ GroupMenter/modules/__musicals.py
@@ -1,7 +1,12 @@
 __mod_name__ = "MUSIC"
 
 __help__ = """
- ❍ /song <song name>*:* Uploads the song in it's best quality available
- 💡Ex: `/song Faded Alan Walker`
+ ❍ /play <song name>*:* Play song in vc.
+ ❍ /skip *:* Skip the song from vc.
+ ❍ /pause *:* Pause song from vc.
+ ❍ /resume *:* Resume song from vc.
+ ❍ /song <song name>*:* Download song.
+ 💡Ex: `/song Amplifier`
  ❍ /saavn <song name>*:* Download song from saavn.
+ ❍ /end *:* End song from vc 
 """
